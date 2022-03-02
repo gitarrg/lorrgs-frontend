5a374954b49a0ceb3ee00432c7d9fc092e6f4844
arrg <arrg@lorrgs.io>
2022-03-02 14:29:00 +0000

    tmp

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@ export const DISCORD_LINK = "https://discord.gg/jZWj6djJk2"
 export const PATREON_LINK = "https://www.patreon.com/lorrgs"
 export const BUYMEACOFFEE_LINK = "https://www.buymeacoffee.com/lorrgs"
 
-export const ZONE_ID = 28
+export const ZONE_ID = 29
 
 
 // Timeline Vars
