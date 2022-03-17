a553651cbf02d36e48de51baa8b424cb6d530f18
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2022-03-17 15:50:36 +0000

    add option to show/hide deaths. (hide by default for spec rankings / show by default for user reports)

--- src/components/Timeline/Resurrection.ts
+++ src/components/Timeline/Resurrection.ts
@@ -1,4 +1,5 @@
 import EventLine from "./EventLine"
+import * as constants from "./constants"
 
 
 export default class Resurrection extends EventLine {
@@ -14,5 +15,14 @@ export default class Resurrection extends EventLine {
     _get_text_tooltip() {
         const t = this._get_tooltip_elements()
         return `${t.icon} ${t.time} ${t.source} resurrects ${t.player} with ${t.spell}`
+    }
+
+    _handle_display_settings(settings: { [key: string]: boolean}) {
+        this.visible(settings.show_deaths)
+    }
+
+    handle_event(event_name: string, payload: any) {
+        if (event_name === constants.EVENT_DISPLAY_SETTINGS) { this._handle_display_settings(payload)}
+        super.handle_event(event_name, payload)
     }
 }
