eadc3a065e1a65d00bb09d8a7f812d5dcd95ce10
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2022-03-22 16:28:49 +0000

    add user avatar

--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -3,12 +3,12 @@ import LorrgsLogo from "../assets/LorrgsLogo"
 import styles from "./HeaderLogo.scss"
 
 
-export default function HeaderLogo({wow_class = "wow-boss"}) {
+export default function HeaderLogo({wow_class="wow-boss", size="icon-l"}) {
 
     return (
         <div className={styles.container}>
             <Link to="/">
-                <LorrgsLogo className={`${styles.logo} ${wow_class} icon-l bg-dark rounded grow-when-touched`} />
+                <LorrgsLogo className={`${styles.logo} ${wow_class} ${size} bg-dark rounded grow-when-touched`} />
             </Link>
         </div>
     )
