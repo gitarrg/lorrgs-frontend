03daec4a495022ddfc046e1eb05e957ced40b7e9
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2023-03-13 16:05:32 +0000

    hide add from index

---
+++ src/routes/AdsTest.tsx
@@ -0,0 +1,22 @@
+
+
+import Ad from "../components/Ad"
+
+
+export default function AdsTest() {
+
+    return (
+        <div>
+            <h1>AdsTest</h1>
+            <Ad />
+                <div
+                    style={{
+                        // "height": "30px",
+                        "background": "yellow",
+                    }}
+                >
+                <Ad />
+        </div>
+
+    </div>)
+}
