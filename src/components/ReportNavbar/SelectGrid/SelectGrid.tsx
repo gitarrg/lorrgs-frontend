e214cfd7e08a6eaabbb5456c655d15765c773501
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2024-03-10 15:08:57 +0000

    Allow selecting all players/pulls

--- src/components/ReportNavbar/SelectGrid/SelectGrid.tsx
+++ src/components/ReportNavbar/SelectGrid/SelectGrid.tsx
@@ -1,4 +1,9 @@
 import type { ReactNode } from "react"
+import { createContext, useState } from "react"
+import useUser from "../../../routes/auth/useUser"
+
+
+export const SelectGridContext = createContext<boolean>(false)
 
 
 type SelectGridProps = {
@@ -8,11 +13,32 @@ type SelectGridProps = {
 
 
 export default function SelectGrid({title="", children} : SelectGridProps) {
+
+
+    const [value, setValue] = useState(false)
+
+    const user = useUser()
+    const user_can_multiselect = user.permissions.includes("user_reports")
+
+    function onClick() {
+        user_can_multiselect && setValue(value => !value)
+    }
+
     return (
         <div className="flex-grow-1">
-            {title && <h4 className="mb-1">{title}</h4>}
+
+            {title &&
+                <h4 className="mb-1" onClick={onClick}>
+                    {/* Subbed users get a clickabe version */}
+                    {user_can_multiselect && <a href="#">{title}</a>}
+                    {!user_can_multiselect && title}
+                </h4>
+            }
+
             <div className="d-flex flex-column gap-3">
-                {children}
+                <SelectGridContext.Provider value={value}>
+                    {children}
+                </SelectGridContext.Provider>
             </div>
         </div>
     )
