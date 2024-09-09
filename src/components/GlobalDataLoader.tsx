3718d85c065421973edf254edb160c489169c0f9
arrg <arrg@lorrgs.io>
2024-09-09 18:20:35 +0100

    update default raid

--- src/components/GlobalDataLoader.tsx
+++ src/components/GlobalDataLoader.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-// import { ZONE_ID } from "../constants";
+import { ZONE_ID } from "../constants";
 import { load_bosses } from "../store/bosses";
 import { load_classes } from "../store/classes";
 import { load_roles } from "../store/roles";
@@ -12,14 +12,7 @@ export default function GlobalDataLoader() {
     const dispatch = useDispatch();
 
     useEffect(() => {
-
-        // dispatch(load_bosses(ZONE_ID));
-
-        // Fated Edit
-        dispatch(load_bosses(31)); // Vault of the Incarnates
-        dispatch(load_bosses(33)); // Aberrus
-        dispatch(load_bosses(35)); // Amirdrassil
-
+        dispatch(load_bosses(ZONE_ID));
         dispatch(load_classes());
         dispatch(load_roles());
         dispatch(load_specs());
